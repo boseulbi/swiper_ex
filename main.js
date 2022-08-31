8675ce5fccb36ef96b9ed9a77e21b808d9abb470
boseulbi <qhseul0916@naver.com>
2022-08-31 14:36:13 +0900

    swiper완성

--- main.js
+++ main.js
@@ -6,4 +6,20 @@ const swiper = new Swiper("main", {
         slidesPerView: "auto", // 숫자 해당갯수단위로 패널보임, "auto": 100%, 특정크기지정
         spaceBetween: 30, //패널사이간격
         centeredSlides: true, //활성화 패널을 항상 가로 가운데 배치
+        pagination: {
+            el: ".swiper-pagination",
+            //clickable: true
+            type: "fraction"
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        effect: "coverflow",
+        coverflowEffect: {
+            rotate: 50,//y축 회전각도
+            stretch: -100,//당겨짐 정도
+            depth: 400,//입체적인 왜곡효과정도
+            slideShadow: false, //패널의 그림자설정
+        }   
     });
